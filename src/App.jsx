43f0bcd7238c9ba67538abf9c73b2bad5615b2cd
jyothsnaviboddu <jyothsnaviboddu@gmail.com>
2026-03-16 21:35:27 +0530

    nestedrotuingnavlink

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import viteLogo from '/vite.svg'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import A from './Components/A.jsx'
 import {myContext} from './Components/Mycontext.js'
+import Counter from './Counter.jsx'
+import Todolist from './Todolist/Todolist.jsx'
+import { Outlet ,Link,NavLink} from 'react-router-dom'
+import Products from './Components/Products/Products.jsx'
 import './App.css'
 
 function App()
@@ -19,9 +23,54 @@ function App()
       alert("Hello Jyothsnavi....!")
   }
   var a="harshitha"
-  return  <myContext.Provider value={{c,a,abc}}>
-            <A/>
-         </myContext.Provider>
+  return  <div>
+          <NavLink
+            to="/counter"
+            className={({ isActive }) => {
+              return isActive
+                ? "bg-info text-light text-decoration-none"
+                : "";
+            }}
+          >
+            Counter
+          </NavLink>
+                  <br></br>
+           <NavLink 
+           to='/todolist' 
+           className={({isActive})=>{
+            return isActive
+            ? "bg-success text-dark text-decoration-none"
+            :""
+           }}
+           >
+           Todolist
+           </NavLink>
+           <br/>
+           <NavLink 
+           to='/stopwatch'
+           className={({isActive})=>
+            {
+                return isActive
+                ?"bg-danger text-light text-decoration-none"
+                :""
+
+           }}>
+            Stopwatch
+           </NavLink>
+           <br/>
+           <NavLink 
+                    to='/products'
+                    className={({isActive})=>{
+                        return isActive
+                        ?"bg-warning text-light text-decoration-none"
+                        :""
+                    }}
+           >
+            Products
+           </NavLink>
+            <Outlet></Outlet>
+           </div>     
+         
           
   
       
